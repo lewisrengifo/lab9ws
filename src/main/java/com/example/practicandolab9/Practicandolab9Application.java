5a2c76ae0ac05ffcc0d2e69784c97a2cde9679b7
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-06-29 16:25:16 -0500

    second commit

---
+++ src/main/java/com/example/practicandolab9/Practicandolab9Application.java
@@ -0,0 +1,13 @@
+package com.example.practicandolab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicandolab9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practicandolab9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicandolab9/Practicandolab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicandolab9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicandolab9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
