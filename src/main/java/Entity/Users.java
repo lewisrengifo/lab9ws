98a5a663f3c261bc7e68df5ed0f8b2b113152c7a
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-06-29 16:38:16 -0500

    entidad users

--- src/main/java/Entity/Users.java
+++ src/main/java/Entity/Users.java
@@ -1,4 +1,73 @@
 package Entity;
 
+import javax.persistence.*;
+@Table(name = "users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String fullname;
+    private String username;
+    private String email;
+    private String password;
+    private int status;
+    @ManyToOne
+    @JoinColumn(name = "idrol")
+    private Roles roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Entity.Roles getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Entity.Roles roles) {
+        this.roles = roles;
+    }
 }
