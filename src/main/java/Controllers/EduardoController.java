a6ae1bd3f8e31d8662ebc41e3709fb8d3700c522
Eduardo Sanchez <a20122509@pucp.pe>
2020-06-29 16:51:04 -0500

    tercer commit

--- src/main/java/Controllers/EduardoController.java
+++ src/main/java/Controllers/EduardoController.java
@@ -1,10 +1,27 @@
 package Controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class EduardoController {
-    
+    @PostMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity guardarProducto(
+            @RequestBody Product product,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        productRepository.save(product);
+        if (fetchId) {
+            responseMap.put("id", product.getId());
+        }
+        responseMap.put("estado", "creado");
+        return new ResponseEntity(responseMap, HttpStatus.CREATED);
+
+    }
+
+
 }
